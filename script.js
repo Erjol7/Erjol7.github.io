c5cefc1557ef99e5291c322b3736ccea64a957b7
Erjol7 <erjolibi@gmail.com>
2025-05-26 18:56:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <form id="quiz-form">
       <!-- Pjesa 1: Pyetjet me përgjigje të hapura (Perkufizo termat dhe plotëso vendet bosh) -->
       <section id="section-1">
-        <h3>1. Perkufizo termat</h3>
+        <h3>I.Stacioni 1</h3>
         <fieldset>
           <legend>1. Perkufizo termat</legend>
           <label>a) Kromozom: <input type="text" name="term1" placeholder="Shkruaj përgjigjen"></label>
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       <!-- Pjesa 2: Pyetjet e teorisë dhe emërtimit -->
       <section id="section-2">
-        <h3>2. Teori dhe Emërtim</h3>
+        <h3>II. Stacioni 2</h3>
         <fieldset>
           <legend>Formulo ligjin e dyte të Mendeleit</legend>
           <input type="text" name="law" placeholder="Shkruaj ligjin">
@@ -102,37 +102,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
       <!-- Pjesa 3: Pyetje me alternativa dhe plotëso -->
       <section id="section-3">
-        <h3>3. Multiple Choice & Plotëso</h3>
+        <h3>III. Stacioni 3</h3>
         <fieldset>
-          <legend>a) Cili është materiali gjenetik që përcakton karakteristikat trasheguese të organizmat? <em>(P:ii)</em></legend>
+          <legend>a) Cili është materiali gjenetik që përcakton karakteristikat trasheguese të organizmat?</legend>
           <label><input type="radio" name="q3a" value="0"> ARN</label>
           <label><input type="radio" name="q3a" value="1"> ADN</label>
           <label><input type="radio" name="q3a" value="2"> m-ARN</label>
           <label><input type="radio" name="q3a" value="3"> t-ARN</label>
         </fieldset>
         <fieldset>
-          <legend>b) Cili shkencëtar hodhi bazat e trashegimit? <em>(P:ii)</em></legend>
+          <legend>b) Cili shkencëtar hodhi bazat e trashegimit? </legend>
           <label><input type="radio" name="q3b" value="0"> Charles Darwin</label>
           <label><input type="radio" name="q3b" value="1"> Gregor Mendel</label>
           <label><input type="radio" name="q3b" value="2"> Francis Crick</label>
           <label><input type="radio" name="q3b" value="3"> James Watson</label>
         </fieldset>
         <fieldset>
-          <legend>c) Cili është roli i kromozomeve në qelizë? <em>(P:ii)</em></legend>
+          <legend>c) Cili është roli i kromozomeve në qelizë? </legend>
           <label><input type="radio" name="q3c" value="0"> Prodhimi i energjisë</label>
           <label><input type="radio" name="q3c" value="1"> Ruajtja e informacionit gjenetik</label>
           <label><input type="radio" name="q3c" value="2"> Transporti i molekulave</label>
           <label><input type="radio" name="q3c" value="3"> Shkatërrimi i substancave të dëmshme</label>
         </fieldset>
         <fieldset>
-          <legend>d) Cili është rezultati i një kryqëzimi midis dy individëve heterozigotë për një tipar të dhënë? <em>(P:iiii)</em></legend>
+          <legend>d) Cili është rezultati i një kryqëzimi midis dy individëve heterozigotë për një tipar të dhënë? </legend>
           <label><input type="radio" name="q3d" value="0"> 100% homozigotë</label>
           <label><input type="radio" name="q3d" value="1"> 50% heterozigotë dhe 50% homozigotë</label>
           <label><input type="radio" name="q3d" value="2"> 75% homozigotë dhe 25% heterozigotë</label>
           <label><input type="radio" name="q3d" value="3"> 25% homozigotë dominues, 50% heterozigotë, dhe 25% homozigotë recesiv</label>
         </fieldset>
         <fieldset>
-          <legend>e) Cili është roli i ARN-së në qelizë? <em>(P:i)</em></legend>
+          <legend>e) Cili është roli i ARN-së në qelizë? </legend>
           <label><input type="radio" name="q3e" value="0"> Ruajtja e informacionit gjenetik</label>
           <label><input type="radio" name="q3e" value="1"> Prodhimi i proteinave</label>
           <label><input type="radio" name="q3e" value="2"> Shkatërrimi i substancave të dëmshme</label>
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <label><input type="radio" name="q3f" value="3"> Ruajtja e proteinave në qelizë</label>
         </fieldset>
         <fieldset>
-          <legend>g) Jepen bazat e ADN A; T; G; A. Si do të jetë renditja e bazave në antikodonin e t-ARN? <em>(P:ii)</em></legend>
+          <legend>g) Jepen bazat e ADN A; T; G; A. Si do të jetë renditja e bazave në antikodonin e t-ARN? </legend>
           <label><input type="radio" name="q3g" value="0"> U; A; C; U</label>
           <label><input type="radio" name="q3g" value="1"> A; U; C; A</label>
           <label><input type="radio" name="q3g" value="2"> A; T; G; A</label>
@@ -160,14 +160,14 @@ document.addEventListener("DOMContentLoaded", function () {
           <label><input type="radio" name="q3h" value="3"> 22</label>
         </fieldset>
         <fieldset>
-          <legend>j) Qeliza diploide tek elefantet kanë 56 kromozome. Sa kromozome autosome janë në një gamet? <em>(P:ii)</em></legend>
+          <legend>j) Qeliza diploide tek elefantet kanë 56 kromozome. Sa kromozome autosome janë në një gamet? </legend>
           <label><input type="radio" name="q3j" value="0"> 28</label>
           <label><input type="radio" name="q3j" value="1"> 27</label>
           <label><input type="radio" name="q3j" value="2"> 26</label>
           <label><input type="radio" name="q3j" value="3"> 54</label>
         </fieldset>
         <fieldset>
-          <legend>m) Një qelizë në profazën I të mejozës ka 12 kromatide. Sa tetrade ka qelizë në mejozën 1? <em>(P:i)</em></legend>
+          <legend>m) Një qelizë në profazën I të mejozës ka 12 kromatide. Sa tetrade ka qelizë në mejozën 1? </legend>
           <label><input type="radio" name="q3m" value="0"> 3</label>
           <label><input type="radio" name="q3m" value="1"> 6</label>
           <label><input type="radio" name="q3m" value="2"> 12</label>
@@ -185,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       <!-- Pjesa 4: Pyetje të hapura (open) -->
       <section id="section-4">
-        <h3>4. Pyetje të Hapura</h3>
+        <h3>IV.Stacioni 4 (Pyetje të Hapura)</h3>
         <fieldset>
           <legend>Cfare është translatimi dhe si ndodh?</legend>
           <textarea name="q4_1" rows="3" placeholder="Shkruaj përgjigjen"></textarea>
@@ -206,13 +206,13 @@ document.addEventListener("DOMContentLoaded", function () {
           <legend>Pershkruaj mejozën 2</legend>
           <textarea name="q4_5" rows="3" placeholder="Shkruaj përgjigjen"></textarea>
         </fieldset>
-        <h4>Për ushtrimet shtesë (shkruani përgjigjet në fletë – nuk gjykohen automatik):</h4>
+        <h4>Për ushtrimet shtesë (shkruani përgjigjet në fletë – nuk gjykohen automatikisht):</h4>
         <fieldset>
-          <legend>a) Në një molekulë ADN përmban 500 timine nucleotide. Numri i lidhjeve hidrogjenore midis dy zinxhirëve të saj është 2200. Sa nukleotide ka gjithsej kjo molekulë ADN-je?</legend>
+          <legend>a)Një molekulë ADN përmban 500 timine nukleotide. Numri i lidhjeve hidrogjenore midis dy zinxhirëve të saj është 2200. Sa nukleotide ka gjithsej kjo molekulë ADN-je?</legend>
           <input type="text" name="q4_a" placeholder="Përgjigje">
         </fieldset>
         <fieldset>
-          <legend>b) Në një molekulë ADN-je, sasia e Adenines është sa dyfish i Guanines. Kjo molekulë ka 1200 nukleotide. Përcaktoni numrin e LH në këtë molekulë ADN-je.</legend>
+          <legend>b) Në një molekulë ADN-je, sasia e Adenines është sa dyfish i Guanines. Kjo molekulë ka 1200 nukleotide. Përcaktoni numrin e L.H në këtë molekulë ADN-je.</legend>
           <input type="text" name="q4_b" placeholder="Përgjigje">
         </fieldset>
         <fieldset>
@@ -290,7 +290,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <p>Ligji i zberthimit të tipareve (Nga kryqëzimi i individeve heterozigotë në F1 fitohen klasa fenotipike në raportin 3:1).</p>
           <p>a) Pjesa e figurës: kromozomi X dhe kromozomi Y</p>
           <p>b) Pjesa tjetër: Kariotip femeror</p>
-          <p><em>Shënim: Për përgjigjet e hapura, do të dërgohen në email për verifikim.</em></p>
+          <p><em>Shënim: Për përgjigjet e hapura, do të kontrollohen ne flete zgjidhja .</em></p>
         `;
         const keySection3 = `
           <h4>Çelësi për Pjesën 3 (Multiple Choice & Plotëso):</h4>
@@ -331,17 +331,6 @@ document.addEventListener("DOMContentLoaded", function () {
           ${keySection4}
         `;
         document.getElementById("result").innerHTML = resultHTML;
-
-        // Dërgo të dhënat via EmailJS!
-        emailjs
-          .send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", data)
-          .then(
-            function (response) {
-              alert("Përgjigjet u dërguan me sukses në emailin e mësueses.");
-            },
-            function (error) {
-              alert("Gabim gjatë dërgimit të përgjigjeve.", error);
-            }
           );
       });
     }
